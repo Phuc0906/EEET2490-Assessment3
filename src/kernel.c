41da40132409ec640e2e5affeb87237b2742569e
Phuc Hoang <phuchoang022002@gmail.com>
2023-08-25 23:53:34 +0700

    Display demo image

--- src/kernel.c
+++ src/kernel.c
@@ -15,7 +15,21 @@ void main()
     drawRectARGB32(150, 150, 400, 400, 0x0000BB00, 1); // GREEN
     drawRectARGB32(200, 200, 400, 400, 0x000000CC, 1); // BLUE
     drawRectARGB32(250, 250, 400, 400, 0x00FFFF00, 1); // YELLOW
-    drawPixelARGB32(300, 300, 0x00FF0000);             // RED
+    drawPixelARGB32(500, 900, 0x00FF0000);             // RED
+
+    printImg();
+    // for (int i = 0; i < 200; i++) {
+    //     drawPixelARGB32(200 + i, 900, 0x00AA0000);
+    // }
+
+    // for (int y = 0; y <= 200; y++) {
+    //     for (int x = 0; x <= 200; x++)
+    //     {
+    //         drawPixelARGB32(500 + x, 500 + y, 0x00AA0000);
+    //     }
+    // }
+
+    
 
     while (1)
     {
